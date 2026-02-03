360b7b750d946100500265cd05981cebb286b968
csebyyy <csendesbarna.17@gmail.com>
2026-02-03 10:39:02 +0100

    adószám és szabadnapok hozzáadása

--- VMSProject/EmployeeViewModel.cs
+++ VMSProject/EmployeeViewModel.cs
@@ -3,5 +3,7 @@
     internal class EmployeeViewModel
     {
         public string Name { get; } = "Nagy Anna";
+        public string Adoszam { get; } = "3435867455";
+        public int Szabadnap { get; } = 15;
     }
 }
