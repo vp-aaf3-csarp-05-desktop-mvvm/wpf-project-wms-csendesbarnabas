06bef7df2362c93fad1f37a0a15137d5f5f92549
csebyyy <csendesbarna.17@gmail.com>
2026-02-10 10:24:50 +0100

    viewmodel három propertyvel és ipari megjegyzéssel

--- VMSProject/EmployeeViewModel.cs
+++ VMSProject/EmployeeViewModel.cs
@@ -1,9 +1,23 @@
 ﻿namespace VMSProject
 {
+    /// <summary>
+    /// Dolgozó adatok megjelenítéshez (szabadnapkezelés)
+    /// </summary>
     internal class EmployeeViewModel
     {
+        /// <summary>
+        /// Dolgozó neve
+        /// </summary>
         public string Name { get; } = "Nagy Anna";
-        public string Adoszam { get; } = "3435867455";
-        public int Szabadnap { get; } = 15;
+
+        /// <summary>
+        /// Adószám
+        /// </summary>
+        public string TaxNumber { get; } = "12345678-1-42";
+
+        /// <summary>
+        /// Kivett szabadnapok száma
+        /// </summary>
+        public int DaysOff { get; } = 6;
     }
 }
